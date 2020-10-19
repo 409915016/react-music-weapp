b6ed4333f93e81d56db1a28355543c3f95b8c315
mather <misaka@misaka.im>
2020-10-20 00:30:30 +0800

    使用 State Hook 重构首页

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,135 +1,128 @@
-import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import Taro from '@tarojs/taro'
+import React, {useState, useEffect, useLayoutEffect} from 'react'
+import {View, Text}                                  from '@tarojs/components'
+import Taro                                          from '@tarojs/taro'
 
 import './index.scss'
-import books   from '../../assets/books.json'
-import sources from '../../assets/index.json'
-import chapters from '../../assets/chapters.json'
-
-import {playAudio} from '../../utils'
+import _books                                        from '../../assets/books.json'
+import _sources                                      from '../../assets/index.json'
+import _chapters                                     from '../../assets/chapters.json'
+import Navbar                                        from "../../component/Navbar";
 
 const backgroundAudioManager = Taro.getBackgroundAudioManager()
 
-export default class Index extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      books,
-      sources,
+const pauseMusic = () => {
+  Taro.setStorage({
+    key : 'playing',
+    data: false
+  })
+}
+
+const onPlay = (song) => {
+  Taro.setStorage({
+    key : 'playing',
+    data: true
+  })
+
+  try {
+    const {title, url, thumb}          = song
+    backgroundAudioManager.title       = title
+    backgroundAudioManager.src         = url
+    backgroundAudioManager.coverImgUrl = thumb
+  } catch (err) {
+    console.log('err', err)
+  }
+}
+
+const ContentItemClickHandle = ({book_id}) => {
+
+  Taro.getBackgroundAudioPlayerState({
+    success(res) {
+      if (res.status !== 2) { //played on background
+        Taro.setStorage({
+          key : 'playing',
+          data: true
+        })
+      } else {
+        onPlay(chapters[0])
+      }
+    },
+    fail(res) {
+      Taro.setStorage({
+        key : 'playing',
+        data: false
+      })
     }
-  }
-  pauseMusic(){
-    Taro.setStorage({
-      key: 'playing',
-      data: false
-    })
-  }
-  onPlay(song) {
-    Taro.setStorage({
-      key: 'playing',
-      data: true
-    })
+  })
 
-    try {
-      const {title, url, thumb}          = song
-      backgroundAudioManager.title       = title
-      backgroundAudioManager.src         = url
-      backgroundAudioManager.coverImgUrl = thumb
-    } catch (err) {
-      console.log('err', err)
-    }
-  }
+  Taro.navigateTo({url: `/pages/player/index?book_id=${book_id}&from=index`})
+}
 
-  componentDidMount () {
+function Index() {
+  const [books, SetBooks]       = useState(_books)
+  const [sources, SetSources]   = useState(_sources)
+  const [chapters, SetChapters] = useState(_chapters)
 
-    const that = this
+  useLayoutEffect(() => {
 
     backgroundAudioManager.onStop(() => {
       this.pauseMusic()
     })
 
-    // const book_id = 1
-    // Taro.navigateTo({ url: `/pages/player/index?book_id=${book_id}` })
     Taro.getBackgroundAudioPlayerState({
-      success (res) {
+      success(res) {
         if (res.status !== 2) { //played on background
           Taro.setStorage({
-            key: 'playing',
+            key : 'playing',
             data: true
           })
         } else {
-          that.onPlay(chapters[0])
+          onPlay(chapters[0])
         }
       },
-      fail (res) {
-        that.onPlay(chapters[0])
+      fail(res) {
+        onPlay(chapters[0])
       }
     })
-  }
+  })
 
-  ContentItemClickHandle({book_id}){
-    const that = this
-
-    Taro.getBackgroundAudioPlayerState({
-      success (res) {
-        if (res.status !== 2) { //played on background
-          Taro.setStorage({
-            key: 'playing',
-            data: true
-          })
-        } else {
-          that.onPlay(chapters[0])
-        }
-      },
-      fail (res) {
-        Taro.setStorage({
-          key: 'playing',
-          data: false
-        })
-      }
-    })
-
-    Taro.navigateTo({ url: `/pages/player/index?book_id=${book_id}&from=index` })
-  }
-
-  render () {
-    const {books, sources} = this.state
-
-    return (
-        <View className="wrapper">
-          <View className="header">
-            <View className="header__title">
-              <Text>{ sources.title }</Text>
-            </View>
-            <View className="at-row at-row__align--center at-row__justify--between header__desc">
-              <View className='at-col'>
-                <Text>{ sources.desc }</Text>
-              </View>
-              <View className='at-col at-col-1 at-col--auto'>
-                <Text className='icon iconfont icon-book'>
-                </Text>
-                <Text className='book-text'>共{ sources.chapters || books.length }本</Text>
-              </View>
-            </View>
-          </View>
-          <View className="content">
-            {
-              books.map(i =>{
-                return (
-                    <View className="content-item" onClick={()=>{ this.ContentItemClickHandle({book_id: i.id}) }}>
-                      <View className="content-item__cover">
-                        <image mode="aspectFit" src={i.cover_url} alt=""/>
-                      </View>
-                      <View className="content-item__content">
-                        <View className="content-item__content__title">{ i.title }</View>
-                        <View className="content-item__content__desc">{ i.desc }</View>
-                      </View>
-                    </View>
-                )
-              })
-            }
+  return (
+    <View className="wrapper">
+      <View className="header">
+        <View className="header__title">
+          <Text>{sources.title}</Text>
         </View>
-  </View>)
-  }
+        <View className="at-row at-row__align--center at-row__justify--between header__desc">
+          <View className='at-col'>
+            <Text>{sources.desc}</Text>
+          </View>
+          <View className='at-col at-col-1 at-col--auto'>
+            <Text className='icon iconfont icon-book'>
+            </Text>
+            <Text className='book-text'>共{sources.chapters || books.length}本</Text>
+          </View>
+        </View>
+      </View>
+      <View className="content">
+        {
+          books.map(i => {
+            return (
+              <View className="content-item" onClick={() => {
+                ContentItemClickHandle({book_id: i.id})
+              }}>
+                <View className="content-item__cover">
+                  <image mode="aspectFit" src={i.cover_url} alt=""/>
+                </View>
+                <View className="content-item__content">
+                  <View className="content-item__content__title">{i.title}</View>
+                  <View className="content-item__content__desc">{i.desc}</View>
+                </View>
+              </View>
+            )
+          })
+        }
+      </View>
+    </View>)
+
 }
+
+export default Index
